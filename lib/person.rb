bda5198261e57e2de60683955bd50a86bcd5f783
Allan Glasier <allan@cybernetixdesign.com>
2018-04-15 17:20:29 -0400

    finished lesson

--- lib/person.rb
+++ lib/person.rb
@@ -1,9 +1,30 @@
 class Person
-  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight,
-                :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size,
-                :pant_length, :pant_width
+  attr_accessor :partner, :name
 
   def initialize(attributes)
-    attributes.each {|key, value| self.send("#{key}=", value)}
+    @name = name
+  end
+
+  def get_married(person)
+    self.partner = person
+    if person.class != Person 
+    begin
+      raise PartnerError
+    rescue PartnerError => error
+      puts error.message
+    end
+    else 
+      person.partner = self    
+    end 
+  end
+
+  class PartnerError < StandardError
+    def message
+      "you must give the get_married method an argument of an instance of the person class!"
+    end
   end
 end
+
+beyonce = Person.new("Beyonce")
+beyonce.get_married("Jay-Z")
+puts beyonce.name
